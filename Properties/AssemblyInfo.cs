94a8a9c29fe05dc24d19271598c8640a978631da
Eli Jones <joneseli7395@gmail.com>
2020-10-06 17:29:00 -0400

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TacocaT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TacocaT")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b812cb67-140d-4bbc-b125-0a8260133106")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ scripts/custom.js
@@ -0,0 +1,37 @@
+﻿document.getElementById("flipBtn").addEventListener('click', function () {
+    //Input data
+    let inputWord = document.getElementById("reverseStr").value;
+
+    //Do something with data
+    let lowerInput = inputWord.toLowerCase();
+    let reverseWord = ""
+
+
+    //A string is an array of characters
+    //string word = [w][o][r][d]
+    //               0  1  2  3
+    //.length - 1, otherwise out of execution range 
+    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+        reverseWord += lowerInput.charAt(loop);
+    }
+    let otherReverse = lowerInput.split("").reverse().join("");
+
+    //Output new data
+
+    if (lowerInput === reverseWord) {
+        Swal.fire(
+            "Palindrome!",
+            "Nice! Try another!",
+            'success'
+        )
+    }
+    else {
+        Swal.fire(
+            "Not quite",
+            "Try again!",
+            'error'
+        )
+    }
+    document.getElementById("reverseOutput").innerHTML = reverseWord;
+
+})
